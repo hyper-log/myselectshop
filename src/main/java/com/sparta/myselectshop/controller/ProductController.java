2d411d0c1f0fdc3d41ebc374f98bf718ba0fce6f
hyper <revoluer9@gmail.com>
2025-01-26 21:32:32 +0900

    관심 상품 최저가 API 업데이트

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.service.ProductService;
@@ -18,5 +19,13 @@ public class ProductController {
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
         // 응답 보내기
         return productService.createProduct(requestDto);
+    }
+
+    // 관심 상품 희망 최저가 등록하기
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+
+        // 응답 보내기
+        return productService.updateProduct(id, requestDto);
     }
 }
