367211734248c64df75e530bcdb8c28b315c80d8
hyper <revoluer9@gmail.com>
2025-02-22 23:50:48 +0900

    주석 추가

--- src/main/java/com/sparta/myselectshop/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/myselectshop/config/WebSecurityConfig.java
@@ -61,11 +61,12 @@ public class WebSecurityConfig {
         );
 
         http.authorizeHttpRequests((authorizeHttpRequests) ->
-                authorizeHttpRequests
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
-                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+            authorizeHttpRequests
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .permitAll() // resources 접근 허용 설정
+                .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         http.formLogin((formLogin) ->
@@ -73,7 +74,7 @@ public class WebSecurityConfig {
                         .loginPage("/api/user/login-page").permitAll()
         );
 
-        // 필터 관리
+        // 필터 관리 인가 -> 인증 -> UsernamePasswordAuthentication
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
